fd90a2ca76cb97087a8d70adb171c836ae116420
reevest2 <reevest2@gmail.com>
2021-08-31 18:53:05 -0400

    add reset - GET
    added a reset button.
    added get method
    added results page to display form inputs

--- script.js
+++ script.js
@@ -1,21 +1,8 @@
 "use strict";
 
-//const submitButton = document.getElementById("submit")
+const resultsList = document.getElementById('results')
 
-//submitButton.addEventListener("submit", alert("Huge Success!"));
-
-
-/*
-const submitButton = document.getElementById("submit")
-
-submitButton.onsubmit = alert("Huge success!")
-*/
-
-
-/*
-const submitButton = document.getElementById("submit")
-
-submitButton.addEventListener('click', button => {
-    alert("Huge success!")
+new URLSearchParams(window.location.search).forEach((value, id) => {
+    resultsList.append(`${id}: ${value}`)
+    resultsList.append(document.createElement('br'))
 })
-*/
