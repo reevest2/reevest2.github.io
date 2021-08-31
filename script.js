536ac1d685b9ff476a78654e5f3ae8e42573960f
reevest2 <reevest2@gmail.com>
2021-08-30 22:07:21 -0400

    initial upload

---
+++ script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+//const submitButton = document.getElementById("submit")
+
+//submitButton.addEventListener("submit", alert("Huge Success!"));
+
+
+/*
+const submitButton = document.getElementById("submit")
+
+submitButton.onsubmit = alert("Huge success!")
+*/
+
+
+/*
+const submitButton = document.getElementById("submit")
+
+submitButton.addEventListener('click', button => {
+    alert("Huge success!")
+})
+*/
